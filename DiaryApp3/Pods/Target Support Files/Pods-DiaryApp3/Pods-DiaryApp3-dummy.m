d606bf5a00d4ae84f3fbc3cdc098aa42c82888e1
Kyohei Seo <kyo199333@gmail.com>
2015-07-24 14:00:09 +0900

    Alamofireをインストール

---
+++ DiaryApp3/Pods/Target Support Files/Pods-DiaryApp3/Pods-DiaryApp3-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DiaryApp3 : NSObject
+@end
+@implementation PodsDummy_Pods_DiaryApp3
+@end
---
+++ DiaryApp3/Pods/Target Support Files/Pods-DiaryApp3/Pods-DiaryApp3-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_DiaryApp3VersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_DiaryApp3VersionString[];
+
